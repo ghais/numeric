00a334a15f6257819feaf3ab888be521ed4033a7
Ghais Issa <ghais.issa@gmail.com>
2012-09-20 00:42:42 -0400

    Run go fmt

--- dist/ltqnorm.go
+++ dist/ltqnorm.go
@@ -1,6 +1,6 @@
 package dist
 
-import(
+import (
 	"math"
 )
 
@@ -23,36 +23,36 @@ func InvCumulativeNormalDistribution(p float64) float64 {
 	}
 
 	//Coefficients in rational approximations.
-	a := []float64{ -3.969683028665376e+01,  2.209460984245205e+02, 
-		-2.759285104469687e+02,  1.383577518672690e+02,
-		-3.066479806614716e+01,  2.506628277459239e+00}
-	b := []float64{ -5.447609879822406e+01,  1.615858368580409e+02,
-		-1.556989798598866e+02,  6.680131188771972e+01,
+	a := []float64{-3.969683028665376e+01, 2.209460984245205e+02,
+		-2.759285104469687e+02, 1.383577518672690e+02,
+		-3.066479806614716e+01, 2.506628277459239e+00}
+	b := []float64{-5.447609879822406e+01, 1.615858368580409e+02,
+		-1.556989798598866e+02, 6.680131188771972e+01,
 		-1.328068155288572e+01}
 	c := []float64{-7.784894002430293e-03, -3.223964580411365e-01,
 		-2.400758277161838e+00, -2.549732539343734e+00,
-		4.374664141464968e+00,  2.938163982698783e+00}
-	d := []float64{ 7.784695709041462e-03,  3.224671290700398e-01,
-		2.445134137142996e+00,  3.754408661907416e+00}
+		4.374664141464968e+00, 2.938163982698783e+00}
+	d := []float64{7.784695709041462e-03, 3.224671290700398e-01,
+		2.445134137142996e+00, 3.754408661907416e+00}
 
 	// Define break-points.
-	plow  := 0.02425
-	phigh  := 1 - plow
+	plow := 0.02425
+	phigh := 1 - plow
 
 	// Rational approximation for lower region:
-	if p < plow{
-		q  := math.Sqrt(-2*math.Log(p))
-		return (((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q+c[5]) / ((((d[0]*q+d[1])*q+d[2])*q+d[3])*q+1)
+	if p < plow {
+		q := math.Sqrt(-2 * math.Log(p))
+		return (((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q + c[5]) / ((((d[0]*q+d[1])*q+d[2])*q+d[3])*q + 1)
 	}
 
-    // Rational approximation for upper region:
+	// Rational approximation for upper region:
 	if phigh < p {
-		q  := math.Sqrt(-2*math.Log(1-p))
-		return -(((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q+c[5]) / ((((d[0]*q+d[1])*q+d[2])*q+d[3])*q+1)
+		q := math.Sqrt(-2 * math.Log(1-p))
+		return -(((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q + c[5]) / ((((d[0]*q+d[1])*q+d[2])*q+d[3])*q + 1)
 	}
 
-    //Rational approximation for central region:
-    q := p - 0.5
-    r := q*q
-    return (((((a[0]*r+a[1])*r+a[2])*r+a[3])*r+a[4])*r+a[5])*q / (((((b[0]*r+b[1])*r+b[2])*r+b[3])*r+b[4])*r+1)
+	//Rational approximation for central region:
+	q := p - 0.5
+	r := q * q
+	return (((((a[0]*r+a[1])*r+a[2])*r+a[3])*r+a[4])*r + a[5]) * q / (((((b[0]*r+b[1])*r+b[2])*r+b[3])*r+b[4])*r + 1)
 }
--- dist/ltqnorm_test.go
+++ dist/ltqnorm_test.go
@@ -1,25 +1,24 @@
 package dist
 
-import(
+import (
 	"testing"
 )
 
-
 func TestInvCumulativeNormalDistribution(t *testing.T) {
 	delta := 0.00000001
-	if x:= InvCumulativeNormalDistribution(0.00000000001); (x - -6.70602314994) > delta {
+	if x := InvCumulativeNormalDistribution(0.00000000001); (x - -6.70602314994) > delta {
 		t.Error(x)
 	}
-	if x:= InvCumulativeNormalDistribution(0.2); (x -  -0.841621232727) > delta {
+	if x := InvCumulativeNormalDistribution(0.2); (x - -0.841621232727) > delta {
 		t.Error(x)
 	}
-	if x:= InvCumulativeNormalDistribution(0.5); x != 0 {
+	if x := InvCumulativeNormalDistribution(0.5); x != 0 {
 		t.Error(x)
 	}
-	if x:= InvCumulativeNormalDistribution(0.6); (x - 0.25334710286) > delta {
+	if x := InvCumulativeNormalDistribution(0.6); (x - 0.25334710286) > delta {
 		t.Error(x)
 	}
-	if x:= InvCumulativeNormalDistribution(0.999999999999); (x - 7.03448690268) > delta {
+	if x := InvCumulativeNormalDistribution(0.999999999999); (x - 7.03448690268) > delta {
 		t.Error(x)
 	}
 }
